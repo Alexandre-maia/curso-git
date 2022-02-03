157917278c4489097d20b57920a99a72bfd7572d
Alexandre Maia <alexandre--maia@hotmail.com>
2022-02-03 16:31:16 -0300

    Acrecenta a mensagem de despedida

--- Teste_Git/Program.cs
+++ Teste_Git/Program.cs
@@ -5,6 +5,7 @@ namespace Teste_Git {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning");
+            Console.WriteLine("GoodBye");
         }
     }
 }
